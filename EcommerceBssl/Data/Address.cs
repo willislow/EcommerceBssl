17b34a0a1189a31ea67dfe7cb395fe8a7112c3b7
willislow <williamskelvinawele@gmail.com>
2020-01-31 11:50:26 +0100

    added Address Model

---
+++ EcommerceBssl/Data/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceBssl.Data
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string State { get; set; }
+        public string ActualAddress { get; set; }
+        public string CustomerID { get; set; }
+    }
+}
